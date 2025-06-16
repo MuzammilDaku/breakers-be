49d86e3c6dd2cf068d747eeffd46cbe7c1c3907d
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-06-16 18:38:47 +0500

    remove env

--- lib/dbConn.ts
+++ lib/dbConn.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/snooker-club';
+const MONGODB_URI = "mongodb+srv://khanbalochmuzammilabbas:khansahib@cluster0.aw7xhsy.mongodb.net/snooker-app";
 
 if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable');
