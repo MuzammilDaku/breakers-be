95a713b739d26fd2094d219b590a0ca76cb3acbc
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-07-04 16:08:46 +0500

    ...

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -1,8 +1,10 @@
-// "use client";
+// app/api/page.tsx
+export const dynamic = "force-dynamic";
+
 import { getApiDocs } from "@/lib/swagger";
 import ReactSwagger from "../api-doc/react-swagger";
 
-export default async function IndexPage() {
+export default async function SwaggerPage() {
   const spec = await getApiDocs();
   return (
     <section className="container">
