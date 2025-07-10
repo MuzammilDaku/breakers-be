5a15cb47ecedd7db77ff53054c54d77f7ae0e271
MuzammilDaku <khanbalochmuzammilabbas@gmail.com>
2025-07-10 20:45:44 +0500

    Update Status Request

--- app/api/table/game-history/route.ts
+++ app/api/table/game-history/route.ts
@@ -8,12 +8,12 @@ export const POST = async (req: Request) => {
     try {
         await dbConn();
         const body = await req.json();
-        if(!body) {
-            return jsonResponse({error:"Provide All Fields"},200)
+        if (!body) {
+            return jsonResponse({ error: "Provide All Fields" }, 200)
         }
 
         const gameHistory = await GameHistory.create(body);
-        return jsonResponse(gameHistory,201)
+        return jsonResponse(gameHistory, 201)
 
     } catch (error) {
         return jsonResponse({ error: "Internal Server Error" }, 200)
@@ -25,7 +25,32 @@ export const GET = async (req: Request) => {
     try {
         await dbConn();
         const gameHistory = await GameHistory.find()
-        return jsonResponse(gameHistory,201)
+        return jsonResponse(gameHistory, 201)
+
+    } catch (error) {
+        return jsonResponse({ error: "Internal Server Error" }, 200)
+    }
+}
+
+export const PUT = async (req: Request) => {
+    try {
+        await dbConn();
+        const body = await req.json();
+        if (!body) {
+            return jsonResponse({ error: "Provide All Fields" }, 200)
+        }
+        const { _id, ...updateData } = body;
+        if (!_id) {
+            return jsonResponse({ error: "_id is required" }, 200)
+        }
+        Object.keys(updateData).forEach(key => {
+            if (updateData[key] === undefined) {
+                delete updateData[key];
+            }
+        });
+
+        const gameHistory = await GameHistory.findByIdAndUpdate(_id, updateData, { new: true });
+        return jsonResponse(gameHistory, 201)
 
     } catch (error) {
         return jsonResponse({ error: "Internal Server Error" }, 200)
